f1d8bf1696803e8014783ec7f91a335aa58bf732
123 <diegobueno22b@gmail.com>
2025-05-04 16:38:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function abrirModal() {
-    document.getElementById('modalCompra').style.display = 'flex';
+function abrirModal(nomeProduto, descricaoProduto, descricaoTexto, imagemProduto) {
+  document.getElementById('modalCompra').style.display = 'flex';
+  document.getElementById('modalNomeProduto').textContent = nomeProduto;
+  document.getElementById('modalDescricao').textContent = descricaoTexto;
+  document.getElementById('modalImagemProduto').src = imagemProduto;
+}
+
+function fecharModal() {
+  document.getElementById('modalCompra').style.display = 'none';
+}
+
+// Fechar modal ao clicar fora
+window.onclick = function(event) {
+  const modal = document.getElementById('modalCompra');
+  if (event.target == modal) {
+    modal.style.display = 'none';
   }
-  
-  function fecharModal() {
-    document.getElementById('modalCompra').style.display = 'none';
-  }
-  
+}
